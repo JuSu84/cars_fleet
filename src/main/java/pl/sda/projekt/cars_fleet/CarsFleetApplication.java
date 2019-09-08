e579490a0da8091608930d62e08b9f2b5a1092f4
Paweł Bajda <pawel.bajda@gmail.com>
2019-09-08 11:11:18 +0200

    Initial commit

---
+++ src/main/java/pl/sda/projekt/cars_fleet/CarsFleetApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.projekt.cars_fleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsFleetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsFleetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/projekt/cars_fleet/CarsFleetApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.projekt.cars_fleet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarsFleetApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
