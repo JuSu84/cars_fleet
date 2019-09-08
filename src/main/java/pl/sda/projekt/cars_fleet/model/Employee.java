1f3a2512ca736577ff59bbc4cdc0c3e138a8cf33
Paweł Bajda <pawel.bajda@gmail.com>
2019-09-08 12:06:32 +0200

    add class and repository

--- src/main/java/pl/sda/projekt/cars_fleet/model/Employee.java
+++ src/main/java/pl/sda/projekt/cars_fleet/model/Employee.java
@@ -1,9 +1,28 @@
 package pl.sda.projekt.cars_fleet.model;
 
-public class Person {
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    Long id;
     private String firstName;
     private String lastName;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
